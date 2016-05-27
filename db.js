7c4463761e1422d108a7ad9132a8e162bebb05a5
Hongjun Choi <hong_jun_choi@brown.edu>
2016-05-28 01:45:17 +0700

    db

--- db.js
+++ db.js
@@ -1,4 +1,3 @@
 var mongoose = require('mongoose');
-
-mongoose.connect('mongodb://localhost/meanDB');
+mongoose.connect('mongodb://scottljy:scottchoi92@ds017553.mlab.com:17553/heroku_w6zmj82b');
 module.exports = mongoose.connection;
